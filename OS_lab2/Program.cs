701f4d396dc8a86fc82be9f2844341b4bc5a1b97
Igor Belov <belovigor40000@gmail.com>
2021-10-07 17:17:49 +0300

    Добавьте файлы проекта.

---
+++ OS_lab2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OS_lab2
+{
+	class Program
+	{
+
+		static Action foo = () => Console.WriteLine("foo");
+
+		static IMenu mainMenu = new Menu("LAB 2: Memory Management", 
+			new IMenuItem[] { 
+				new Menu("Get memory info", new IMenuItem[]{ 
+					new MenuItem("System info", foo),
+					new MenuItem("Global memory status", foo)
+				}),
+				new Menu("Memory region management", new IMenuItem[]{ 
+					new MenuItem("Define memory segment state", foo),
+					new Menu("Reserve region", new IMenuItem[]{ 
+						new MenuItem("Virtual (auto)", foo),
+						new MenuItem("Virtual (define start point)", foo),
+						new MenuItem("Physical (auto)", foo),
+						new MenuItem("Physical (define start point)", foo)
+					}),
+					new MenuItem("Protect region (w-check)", foo),
+					new MenuItem("Free region", foo)
+				}),
+				new MenuItem("Write memory cells", foo)
+			});
+
+		static void Main(string[] args)
+		{
+			mainMenu.Select();
+		}
+	}
+}
